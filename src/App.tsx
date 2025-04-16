d1645f9bf6eb8048391a414016be2834fc3d1371
Hyah-hah! <5till1v1atic@gmail.com>
2025-04-16 22:34:26 +0900

    ⚰️ 不要になったクラス関係を削除

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import { SlantedBackground } from './components/SlantedBackground';
 function App() {
   return (
     <div className="App">
-      <SlantedBackground mainColor="pink" nextColor="yellow" type="header">
+      <SlantedBackground type="header">
         <header>
           <Title order={1} ta="center" mb="lg" className="game-logo">
             ドーナドーナ キャラクターエディター
           </Title>
         </header>
       </SlantedBackground>
-      <SlantedBackground mainColor="cyan" nextColor="yellow" type="normal">
+      <SlantedBackground type="normal">
         <main>
           <CharacterEditor />
         </main>
--- src/components/SlantedBackground.tsx
+++ src/components/SlantedBackground.tsx
@@ -1,28 +1,13 @@
 import styles from '../styles/SlantedBackground.module.css';
 
-type Color = 'yellow' | 'pink' | 'cyan';
 type Type = 'header' | 'normal' | 'footer';
 
 type Props = {
   children: React.ReactNode;
-  mainColor: Color;
-  nextColor: Color;
   type: Type;
 };
 
-export const SlantedBackground = ({ children, mainColor, nextColor, type = 'normal' }: Props) => {
-  const mainColorClassNames: Record<Color, string> = {
-    pink: styles.slantedBGMainColorPink,
-    cyan: styles.slantedBGMainColorCyan,
-    yellow: styles.slantedBGMainColorYellow,
-  };
-
-  const nextColorClassNames: Record<Color, string> = {
-    pink: styles.slantedBGNextColorPink,
-    cyan: styles.slantedBGNextColorCyan,
-    yellow: styles.slantedBGNextColorYellow,
-  };
-
+export const SlantedBackground = ({ children, type = 'normal' }: Props) => {
   const typeClassNames: Record<Type, string> = {
     header: styles.slantedHeaderBG,
     normal: styles.slantedNormalBG,
@@ -30,9 +15,7 @@ export const SlantedBackground = ({ children, mainColor, nextColor, type = 'norm
   };
 
   return (
-    <div
-      className={`${styles.slantedBG} ${mainColorClassNames[mainColor]}  ${nextColorClassNames[nextColor]} ${typeClassNames[type]}`}
-    >
+    <div className={`${styles.slantedBG} ${typeClassNames[type]}`}>
       <div>{children}</div>
     </div>
   );
