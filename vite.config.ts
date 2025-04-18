7d61d2745ef1d311a9beabf936c5867c734c3db6
Hyah-hah! <5till1v1atic@gmail.com>
2025-04-19 02:23:59 +0900

    🚀🔧 CDを整備

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  // CD用に必要な設定
+  base: '/dohna-dohna-character-editor/',
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
   },
-})
+});
