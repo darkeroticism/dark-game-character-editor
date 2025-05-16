c6c3c562bd93e4195e436a98896534e7af32f8cb
Hyah-hah! <5till1v1atic@gmail.com>
2025-05-16 21:22:06 +0900

    ✨ ファイル名をカスタムできるように実装

--- src/components/generate-file-button.tsx
+++ src/components/generate-file-button.tsx
@@ -2,14 +2,19 @@ import { Button, Container } from '@mantine/core';
 import styles from '../styles/parallelogram-button.module.css';
 
 type GenerateFileButtonProps = {
-  onClick: () => void;
+  onClick: (fileName?: string) => void;
+  fileName?: string;
 };
 
-export const GenerateFileButton = ({ onClick }: GenerateFileButtonProps) => (
+export const GenerateFileButton = ({ onClick, fileName }: GenerateFileButtonProps) => (
   <div style={{ textAlign: 'center', marginTop: 30 }}>
     <Container>
       <div className={styles.parallelogramButton}>
-        <Button onClick={onClick} size="lg" style={{ background: 'transparent', border: 'none' }}>
+        <Button 
+          onClick={() => onClick(fileName)} 
+          size="lg" 
+          style={{ background: 'transparent', border: 'none' }}
+        >
           .txtファイルを生成してダウンロード
         </Button>
       </div>
